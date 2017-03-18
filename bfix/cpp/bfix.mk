751f7f25e8f87d6209be8d754dba4fded5482b56
Richard A Hogaboom <richard.hogaboom@gmail.com>
2017-03-18 12:42:28 -0400

    Changes related to the 1.0.5 release.

--- bfix/cpp/bfix.mk
+++ bfix/cpp/bfix.mk
@@ -6,14 +6,10 @@ set -v
 
 if [ "$1" == 'clean' ]
 then
-    rm -f bfix.o
-    rm -f bfix_test.o
+    rm -f *.o
     rm -f bfix_test
     rm -f bfix_test.results
-    rm -f libbfix.a
-    rm -f libbfix.so
-    rm -f libbfix.so.1
-    rm -f libbfix.so.1.0.1
+    rm -f libbfix.*
     rm -f scan-build.bfix.cpp.sa
     exit
 fi
@@ -35,12 +31,10 @@ export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 $CC -$O -c -fpic -ansi -pedantic -Wall -o bfix.o bfix.cpp
 
 # build static library
-rm -rf libbfix.a
 ar rcv libbfix.a bfix.o
 ar ts libbfix.a
 
 # build dynamic library and set up links
-rm -rf libbfix.so.1.0.1
 $CC -$O -shared -Wl,-soname,libbfix.so.1 -o libbfix.so.1.0.1 bfix.o -lc
 ln -sf libbfix.so.1.0.1 libbfix.so.1
 ln -sf libbfix.so.1.0.1 libbfix.so
